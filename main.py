fd7244f70bdf328a5fe889c3d4842cd81dd64e1b
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-11-06 18:35:24 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-
+import os
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import RobustScaler, LabelEncoder
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.ensemble import RandomForestRegressor
+from flask import Flask, jsonify, render_template
+import matplotlib.pyplot as plt
+import io
+import base64
